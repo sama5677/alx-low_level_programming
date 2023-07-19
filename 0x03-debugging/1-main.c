19da46204fdfb242ed92ad9fa9031beaf23b66aa
sama5677 <samaamohamed687@gmail.com>
2023-07-19 09:41:34 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -9,8 +9,48 @@ int main(void)
 {
 	int i;
 
+	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	positive_or_negative(i);
+
+	/*while (i < 10) */
+	/*{ */
+	/*      putchar(i);*/
+	/*} */
+
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
+
+2-largest_number.c
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
+
