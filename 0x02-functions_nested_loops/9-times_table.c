48273b6f5b2818445ced599d53e09048558ee04c
sama5677 <samaamohamed687@gmail.com>
2023-07-18 19:37:39 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,23 @@
 #include "main.h"
 
-
-/***
- * times_table - print the 9 times table
+/**
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
-void times_table(void)
+void jack_bauer(void)
 {
-	int num, mult, prod;
+ int hour, minute;
 
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num ★ mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
+ for (hour = 0; hour <= 23; hour++)
+ {
+  for (minute = 0; minute <= 59; minute++)
+  {
+   _putchar((hour / 10) + '0');
+   _putchar((hour % 10) + '0');
+   _putchar(':');
+   _putchar((minute / 10) + '0');
+   _putchar((minute % 10) + '0');
+   _putchar('\n');
+  }
+ }
 }
